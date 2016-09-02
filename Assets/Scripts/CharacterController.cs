a8d09ddd92ea738cb6010cfcaa1e1e1c21a70a07
tadakun <kokoroko.arive@gmail.com>
2016-09-03 00:20:30 +0900

    [追加]キャラクター移動

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float moveSpeed = 0.1f;
+
+    void OnMouseDrag()
+    {
+        Vector3 objectPointInScreen
+            = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        Vector3 mousePointInScreen
+            = new Vector3(Input.mousePosition.x - moveSpeed,
+                          Input.mousePosition.y,
+                          objectPointInScreen.z);
+
+        Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+        mousePointInWorld.y = this.transform.position.y;
+        mousePointInWorld.z = this.transform.position.z;
+        this.transform.position = mousePointInWorld;
+    }
+}
---
+++ Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleMove : MonoBehaviour {
+
+    public float moveSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += new Vector3(0,0,-moveSpeed);
+        //this.transform.position = Vector3.Lerp(new Vector3(0, 0, 5), new Vector3(0, 0, -5), Time.deltaTime * 0.2f);
+    }
+}
