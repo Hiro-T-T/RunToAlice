1d2a44a84451c2e637574fe0d1a71adf4e463381
plumchang <umezawa@cmc.ss.is.nagoya-u.ac.jp>
2016-09-03 00:17:08 +0900

    背景プレハブ

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGround : MonoBehaviour
+{
+    
+    private float scrollSpeed = 0.5f;
+
+    void Start()
+    {
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", Vector2.zero);
+    }
+
+    void Update()
+    {
+        ScrollTexture();
+    }
+
+    void ScrollTexture()
+    {
+        var y = Mathf.Repeat(Time.time * scrollSpeed, 1);
+
+        var offset = new Vector2(0, y);
+
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
