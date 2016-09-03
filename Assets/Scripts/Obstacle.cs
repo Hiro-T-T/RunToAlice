22bd78c7b03df30a67dc8a311b77e5113e2e7c05
plumchang <umezawa@cmc.ss.is.nagoya-u.ac.jp>
2016-09-03 10:52:55 +0900

    [追加]障害物クラス

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ObsType { Stone, Braver, Slime };
+
+public class Obstacle
+{
+
+    public ObsType Type;
+    public int HP;
+    public bool isBreakable;
+    public float X, Y, Z;
+
+    public Obstacle()
+    {
+
+    }
+
+    public Obstacle(ObsType type, int hp, bool isbreakable, float x, float y, float z)
+    {
+        Type = type;
+        HP = hp;
+        isBreakable = isbreakable;
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
