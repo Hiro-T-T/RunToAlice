e75e60aa73f97093bd611a3df3eb85166d25902b
plumchang <umezawa@cmc.ss.is.nagoya-u.ac.jp>
2016-09-05 10:58:34 +0900

    game1script

--- Assets/Scripts/Game1Script.cs
+++ Assets/Scripts/Game1Script.cs
@@ -5,7 +5,7 @@ public class Game1Script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Instantiate(Resources.Load("alice"));
+       
         Instantiate(Resources.Load("background"));
         Instantiate(Resources.Load("background"), new Vector3(0, 0, 10), Quaternion.identity);
         Instantiate(Resources.Load("background"), new Vector3(0, 0, 20), Quaternion.identity);
@@ -13,9 +13,6 @@ public class Game1Script : MonoBehaviour {
         Instantiate(Resources.Load("background"), new Vector3(0, 0, 40), Quaternion.identity);
         Instantiate(Resources.Load("background"), new Vector3(0, 0, 50), Quaternion.identity);
         Instantiate(Resources.Load("endWall"));
-        Instantiate(Resources.Load("game1_canvas"));
-        Instantiate(Resources.Load("GameManager"));
-        
 
     }
 
