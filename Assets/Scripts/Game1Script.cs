f49054d5daa422c2980b1f3a3d70a1651b9b9ebc
plumchang <umezawa@cmc.ss.is.nagoya-u.ac.jp>
2016-09-05 11:42:43 +0900

    [修正]まてりあるとか

--- Assets/Scripts/Game1Script.cs
+++ Assets/Scripts/Game1Script.cs
@@ -5,19 +5,19 @@ public class Game1Script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-       
-        Instantiate(Resources.Load("background"));
-        Instantiate(Resources.Load("background"), new Vector3(0, 0, 10), Quaternion.identity);
-        Instantiate(Resources.Load("background"), new Vector3(0, 0, 20), Quaternion.identity);
-        Instantiate(Resources.Load("background"), new Vector3(0, 0, 30), Quaternion.identity);
-        Instantiate(Resources.Load("background"), new Vector3(0, 0, 40), Quaternion.identity);
-        Instantiate(Resources.Load("background"), new Vector3(0, 0, 50), Quaternion.identity);
-        Instantiate(Resources.Load("endWall"));
+	   
+		Instantiate(Resources.Load("background"));
+		Instantiate(Resources.Load("background"), new Vector3(0, 0, 10), Quaternion.identity);
+		Instantiate(Resources.Load("background"), new Vector3(0, 0, 20), Quaternion.identity);
+		Instantiate(Resources.Load("background"), new Vector3(0, 0, 30), Quaternion.identity);
+		Instantiate(Resources.Load("background"), new Vector3(0, 0, 40), Quaternion.identity);
+		Instantiate(Resources.Load("background"), new Vector3(0, 0, 50), Quaternion.identity);
+		Instantiate(Resources.Load("endWall"));
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update () {
+	// Update is called once per frame
+	void Update () {
 	
 	}
 }
